4f7aca6b1227d6dcabcbc62e56f6f2ef928434fa
Gonxo <gmartinroldan@gmail.com>
2017-03-01 15:27:06 +0000

    obtaining weather data code

--- obtaining_data.py
+++ obtaining_data.py
@@ -2,6 +2,8 @@ import requests
 import datetime
 import numpy as np
 import pandas as pd
+import time
+
 
 # Set up the destination directory
 dataDir = 'DATA_DIRECTORY'
@@ -10,49 +12,60 @@ dataDir = 'DATA_DIRECTORY'
 # Writting and reading Apikeys are needed in case there is not a reading apikey bypass. 
 # Direct to 'apiKeyDictionary.csv' location.
 secretsDir = 'SECRETS_DIRECTORY'
-apiDic = pd.read_csv(secretsDir+'/apiKeyDictionary.csv')
+apiDic = pd.read_csv('~\\ML-energy-use\\SECRETS_DIRECTORY\\apiKeyDictionary.csv')
 types = np.unique(apiDic['type'])
 
 # File with security secrets stored
-secrets = pd.read_json(secretsDir+'/secrets.json')
+#secrets = pd.read_json('~\\ML-energy-use\\SECRETS_DIRECTORY\\secrets.json')
 
 # Creating a saving file
-store = pd.HDFStore(dataDir+'/'+str(datetime.datetime.now())+'_feeds.h5')
+store = pd.HDFStore('C:\\Users\\Gonxo\\ML-energy-use\\DATA_DIRECTORY\\feb_feeds.h5')
 
 # Looping for all types of feed
 for type in types:
 
-	for index, row in apiDic.loc[(apiDic['type']==type),['key','id']].iterrows():
+    for index, row in apiDic.loc[(apiDic['type']==type),['key','id']].iterrows():
 		
 		# NOTE: adding troubleshooting might be interesting. 
 		# Obtaining streamed data
-		response = requests.get("https://emoncms.org/feed/export.json?apikey="+str(row['key'])+"&id="+str(row['id'])+"&start=0&CCBYPASS="+secrets['BYPASS'][0])
+        response = requests.get("https://emoncms.org/feed/export.json?apikey="+str(row['key'])+"&id="+str(row['id'])+"&start=0&CCBYPASS=H8F47SBDEJ")
 		
 		# Obtaining meta data for error detection and time generation
-		meta = eval(requests.get("https://emoncms.org/feed/timevalue.json?id="+str(row['id'])+"&apikey="+str(row['key'])).content)
+        meta = eval(requests.get("https://emoncms.org/feed/timevalue.json?id="+str(row['id'])+"&apikey="+str(row['key'])).content)
 
 		# Checking data consistancy in bytes
-		if len(response.content) % 4 == 0:
-			
+        if not len(response.content):
+            print('Error: the feed is empty. Feed: '+ str(row['id']))    
+            
+        elif len(response.content) % 4 == 0 or (len(response.content)-2) % 4 == 0:
+                
 			# Creating a numpy array from data steam. Decodification is done here.
-			array = np.fromstring(response.content, 'float32')
+            if (len(response.content)-2) % 4 == 0:
+                array =  np.fromstring(response.content[2:], 'float32')
+            else:
+                array = np.fromstring(response.content, 'float32')
 			
 			# Finding starting time point. 
 			# It is computed as the time value of the last observation minus the size of the data
-			end_point = int(meta['time'])
-			start_point = end_point - array.size*10
+            end_point = int(meta['time'])
+            start_point = end_point - array.size*10
 			
 			# Checking if the obtained data stream has the same value as the last observation in the server. 
-			if array[-1] != float(meta['value']):
-				print("Error: The last value of the array do not match the last value obtained from the feed: "+str(row['id']))
-	
+            if array[-1] != float(meta['value']):
+                print("Error: The last value of the array do not match the last value obtained from the feed: "+str(row['id']))
+                print('Last value array: '+ str(array[-1]) + '\nLast value feed: '+ str(meta['value']))
+                print(time.time())
 			# Giving time to the data observations
-			df = pd.DataFrame({"unit":array},
-						   index= pd.date_range(pd.to_datetime(start_point+10, unit='s')
-						   ,pd.to_datetime(end_point, unit='s'), freq='10S'))
-								
-		else:
-			print("Error: The number of elements recieved is not multiple of 4. Decodification is not posible. Feed: "+ str(row['id']))
+            df = pd.DataFrame({"unit":array},index= pd.date_range(pd.to_datetime(start_point+10, unit='s'),pd.to_datetime(end_point, unit='s'), freq='10S'))
+            apiDic.loc[index,'start'] = start_point
+            apiDic.loc[index, 'end'] = end_point	
+							
+        else:
+            print("Error: The number of elements recieved is: "+str(len(response.content))+" which is not multiple of 4. Decodification is not posible. Feed: "+ str(row['id']))
 		
+
+         
 		# Saving individual data frames into the hf5 file. Each feed is individually saved with its type.
-		store[str(type)+'_'+str(row['id'])] = df
+        store[str(type)+'_'+str(row['id'])] = df
+
+apiDic.to_csv('~\\ML-energy-use\\SECRETS_DIRECTORY\\apiKeyDictionary2.csv')
---
+++ obtaining_weather_data.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 27 10:47:10 2017
+
+@author: Gonxo
+
+This code imports the historical weather data from weather underground. 
+You need to set up an account and the requests will consume one of the given raindrops, 
+so collect as many as posible in one day. 
+"""
+import requests
+import json
+import pandas as pd
+import datetime as dt
+
+# Put your weather underground apikey
+wuapikey = 'APIKEY'
+
+# Change to your own location.
+coordinates = 'LATITUDE,LONGITUDE'
+
+# Open the metadata
+apiDic = pd.read_csv('~\\ML-energy-use\\SECRETS_DIRECTORY\\apiKeyDictionary.csv')
+
+
+weather = pd.DataFrame()
+
+# Obtain the necesary period
+initdate = dt.datetime.utcfromtimestamp(apiDic.loc[(apiDic['lan_long']==coordinates),['start']].min())
+enddate = dt.datetime.utcfromtimestamp(apiDic.loc[(apiDic['lan_long']==coordinates),['end']].max())
+print(initdate.strftime('%Y%m%d'), enddate.strftime('%Y%m%d'))
+
+# Call to weather underground
+for day in pd.date_range(initdate,enddate, freq= 'd'):
+  
+    day = day.strftime('%Y%m%d')
+    response = requests.get('http://api.wunderground.com/api/'+wuapikey+'/history_'+
+                            str(day)+'/q/'+coordinates+'.json').content
+                            
+
+    decoded = json.loads(response)
+    
+        
+    df = pd.DataFrame(decoded['history']['observations'])
+
+# Uncoment to create a datetime from a list.
+#        for j in range(df['date'].size):
+#            df['date'][j] = pd.datetime(int(df['date'][j]['year']),int(df['date'][j]['mon']),
+#                        int(df['date'][j]['mday']),int(df['date'][j]['hour']),int(df['date'][j]['min']))
+    
+    weather = weather.append(df)
+
+print('Weather')
+print(weather)
+weather.to_csv('~\\ML-energy-use\\DATA_DIRECTORY\\WEATHER_DATA\\'+coordinates+'.csv')
+
+
+# Stop at 500 calls/days.
+
+# Save data 
